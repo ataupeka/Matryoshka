72a6f00f9c99b13c7e3bb0c564e8c5681056d0f9
Ala Taupeka <121459925+ataupeka@users.noreply.github.com>
2023-05-19 19:50:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>{
-   searchForm.classList.toggle('active');
+   searchForm.classList.toggle('search-form__active');
    
 }
+
+let cart = document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick = () =>{
+   cart.classList.toggle('shopping-cart__active');
+   
+}
+
+let loginForm = document.querySelector('.login-form');
+document.querySelector('#user-btn').onclick = () =>{
+   loginForm.classList.toggle('login-form__active');
+   
+}
+
