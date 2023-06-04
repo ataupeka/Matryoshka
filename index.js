8502436ce3ad4ad8261b765507274ec5395ca6a6
Ala Taupeka <121459925+ataupeka@users.noreply.github.com>
2023-06-03 20:05:02 -0400

    Update index.js
    add swiper

--- index.js
+++ index.js
@@ -44,5 +44,14 @@ window.onscroll= () =>{
     navbar.classList.remove('navbar__active');
 }
 
-
+const swiper = new Swiper(".swiper", {
+   effect: "fade",
+   pagination: {
+     el: ".swiper-pagination",
+   },
+   autoplay: {
+     delay: 3000,
+     disableOnInteraction: false,
+   },
+ });
 
